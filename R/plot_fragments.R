647048e4efe5f4bbbc1b96fc8f959fa61947f0bb
TearsWillFall <osvaldas.vainauskas@gmail.com>
2022-01-28 11:16:19 +0000

    Fix

--- R/plot_fragments.R
+++ R/plot_fragments.R
@@ -169,13 +169,7 @@ get_fragments_length <- function(bin_path = "tools/samtools/samtools", bam = "",
         )
       }, mc.cores = threads)
 
-      dat <- dplyr::bind_rows(dat)
-      names(dat) <- c("SAMPLE", "REGION", "SIZE", "COUNT")
-      dat_tmp <- dat %>%
-        dplyr::group_by(SAMPLE, SIZE) %>%
-        dplyr::summarise(COUNT = sum(COUNT))
-      dat_tmp$REGION <- "GENOME"
-      dat <- dplyr::bind_rows(dat, dat_tmp)
+
   }else{
     dat <- parallel::mclapply(1:nrow(ref_data), FUN = function(x) {
       if (verbose) {
@@ -202,6 +196,7 @@ get_fragments_length <- function(bin_path = "tools/samtools/samtools", bam = "",
     dplyr::summarise(COUNT = sum(COUNT))
   dat_tmp$REGION <- "GENOME"
   dat <- dplyr::bind_rows(dat, dat_tmp)
+  write.table(file = paste0(sample_name, "_fragment_length.txt"), dat, quote = FALSE, col.names = TRUE, row.names = FALSE)
 
 }
 
