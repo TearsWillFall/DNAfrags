40fcf4c1e710032b5c5c1b407a2001e4298f7681
TearsWillFall <osvaldas.vainauskas@gmail.com>
2022-03-09 12:18:56 +0000

    Update filter_fragments.R
    Fix

--- R/filter_fragments.R
+++ R/filter_fragments.R
@@ -4,7 +4,7 @@
 #' by size or/and position, or viceversa.
 #'
 #'
-#' @param file Path to the input file.
+#' @param bam Path to the input file.
 #' @param bin_path Path to fastQC executable. Default path tools/samtools/samtools.
 #' @param verbose Enables progress messages. Default False.
 #' @param min_frag_size Minimum fragment size to keep. Default 1.
@@ -13,41 +13,91 @@
 #' @param start_pos Starting position to search within a chromosome. Default 1
 #' @param end_pos Last position to search within a chromosome. Default None
 #' @param bed Path to a BED input file with multiple genome positions. Default None
+#' @param threads Number of threads. Default 3
 #' @export
 
 
-filter_fragments=function(bin_path="tools/samtools/samtools",file="",bed="",min_frag_size=0,max_frag_size=1000000000,chr="",start_pos=1,end_pos="",verbose=FALSE){
+filter_fragments=function(bin_path="tools/samtools/samtools",bam="",bed="",min_frag_size=35,
+  max_frag_size=180, position="",verbose=FALSE,threads=3){
   options(scipen=999)
-  position=""
-  if(bed==""){
-    if (!chr==""){
-        position=paste0("chr",chr)
-      if (!start_pos==1){
-        position=paste0(position,":",start_pos)
-        if (!end_pos==""){
-          position=paste0(position,"-",end_pos)
-          }
 
-      }
-      if (verbose){
-        print(paste(bin_path,"view -h",file,position," | \ awk 'substr($0,1,1)==\"@\""," || ($9>=",min_frag_size,"&& $9<=",max_frag_size,") ||", "($9<=-",min_frag_size,"&& $9>=",max_frag_size,")' | \ samtools view -b >",paste0(ULPwgs::get_sample_name(file),"_",min_frag_size,"-",max_frag_size,"_",position,".bam")))
-      }
-      system(paste(bin_path,"view -h",file,position," | \ awk 'substr($0,1,1)==\"@\""," || ($9>=",min_frag_size,"&& $9<=",max_frag_size,") ||", "($9<=-",min_frag_size,"&& $9>=",max_frag_size,")' | \ samtools view -b >",paste0(ULPwgs::get_sample_name(file),"_",min_frag_size,"-",max_frag_size,"_",position,".bam")))
-    }else{
-      if (verbose){
-        print(paste(bin_path,"view -h",file,position," | \ awk 'substr($0,1,1)==\"@\""," || ($9>=",min_frag_size,"&& $9<=",max_frag_size,") ||", "($9<=-",min_frag_size,"&& $9>=",max_frag_size,")' | \ samtools view -b >",paste0(ULPwgs::get_sample_name(file),"_",min_frag_size,"-",max_frag_size,".bam")))
+  sample_name=ULPwgs::get_sample_name(bam)
+  chrs <- get_chr_names_in_bam(bin_path = bin_path, bam = bam, verbose = verbose)
+  if (bed=="" & position==""){
+    bams <-list()
+    dat <- parallel::mclapply(chrs, FUN = function(x) {
+      if (verbose) {
+        system(paste(bin_path,"view -h",bam,x," | \ awk 'substr($0,1,1)==\"@\""," || ($9>=",
+        min_frag_size,"&& $9<=",max_frag_size,") ||", "($9<=-",min_frag_size,"&& $9>=",max_frag_size,
+        ")' ",bin_path," view -b >",paste0(sample_name,"_",min_frag_size,"_",max_frag_size,".",x,".bam")))
+            }
+      tryCatch(
+        {
+          dat <- system(paste(bin_path,"view -h",bam,x," | \ awk 'substr($0,1,1)==\"@\""," || ($9>=",
+          min_frag_size,"&& $9<=",max_frag_size,") ||", "($9<=-",min_frag_size,"&& $9>=",max_frag_size,
+          ")' ",bin_path," view -b >",paste0(sample_name,"_",min_frag_size,"_",max_frag_size,".",x,".bam")))
 
-      }
-      system(paste(bin_path,"view -h",file,position," | \ awk 'substr($0,1,1)==\"@\""," || ($9>=",min_frag_size,"&& $9<=",max_frag_size,") ||", "($9<=-",min_frag_size,"&& $9>=",max_frag_size,")' | \ samtools view -b >",paste0(ULPwgs::get_sample_name(file),"_",min_frag_size,"-",max_frag_size,".bam")))
+        bams[paste0(sample_name,"_",min_frag_size,"_",max_frag_size,".",x,".bam")] <-
+        paste0(sample_name,"_",min_frag_size,"_",max_frag_size,".",x,".bam")
+              },
+        error = function(e) {
+          return(NULL)
+        }
+      )
+    }, mc.cores = threads)
+
+    ULPwgs::concatenate_bams(bin_path,bams=as.vector(bams),output_name=paste0(sample_name,"_",
+    min_frag_size,"_",max_frag_size),threads=threads)
+    ULPwgs::sort_and_index(bin_path,file=paste0(sample_name,"_",min_frag_size,"_",
+    max_frag_size,".bam"),threads=threads)
+
+
+  }else if (bed!="" & position==""){
+
+    chr_check <- system(paste(bin_path, " view ", bam, " | head -n 1 | awk -F \"\t\" '{print $3}'"), intern = TRUE)
+
+    ref_data <- read.table(bed, comment.char = "",stringsAsFactors=FALSE,colClasses="character")
+
+    if (!grepl("chr", chr_check)) {
+      ref_data[, 1] <- gsub("chr", "", ref_data[, 1])
     }
+
+    dat <- parallel::mclapply(1:nrow(ref_data), FUN = function(x) {
+      if (verbose) {
+        print(paste(bin_path,"view -h",bam, paste0(ref_data[x,1],":",ref_data[x,2],
+        "-",ref_data[x,3])," | \ awk 'substr($0,1,1)==\"@\""," || ($9>=",
+        min_frag_size,"&& $9<=",max_frag_size,") ||", "($9<=-",min_frag_size,"&& $9>=",max_frag_size,
+        ")' | ",bin_path," view -b >",paste0(sample_name,"_",min_frag_size,
+        "-",max_frag_size,"_",paste0(ref_data[x,1],":",ref_data[x,2],"-",ref_data[x,3]),".bam")))
+            }
+      tryCatch(
+        {
+          dat <- system(paste(bin_path,"view -h",bam,paste0(ref_data[x,1],":",ref_data[x,2],
+          "-",ref_data[x,3])," | \ awk 'substr($0,1,1)==\"@\""," || ($9>=",
+          min_frag_size,"&& $9<=",max_frag_size,") ||", "($9<=-",min_frag_size,"&& $9>=",max_frag_size,
+          ")' | ",bin_path," view -b >",paste0(sample_name,"_",min_frag_size,
+          "-",max_frag_size,"_",paste0(ref_data[x,1],":",ref_data[x,2],"-",ref_data[x,3]),".bam")))
+              },
+        error = function(e) {
+          return(NULL)
+        }
+      )
+    }, mc.cores = threads)
+
+  }else if (position!=""& bed==""){
+      if (verbose) {
+        print(paste(bin_path,"view -h",bam,position," | \ awk 'substr($0,1,1)==\"@\""," || ($9>=",
+        min_frag_size,"&& $9<=",max_frag_size,") ||", "($9<=-",min_frag_size,"&& $9>=",max_frag_size,
+        ")' | ",bin_path," view -b >",paste0(sample_name,"_",min_frag_size,"-",max_frag_size,"_",position,".bam")))
+      }
+    system(paste(bin_path,"view -h",bam,position," | \ awk 'substr($0,1,1)==\"@\""," || ($9>=",
+    min_frag_size,"&& $9<=",max_frag_size,") ||", "($9<=-",min_frag_size,"&& $9>=",max_frag_size,
+    ")' | ",bin_path," view -b >",paste0(sample_name,"_",min_frag_size,"-",max_frag_size,"_",position,".bam")))
 
   }else{
-    if (verbose){
-      print(paste(bin_path,"view -h -L",bed,file," | \ awk 'substr($0,1,1)==\"@\""," || ($9>=",min_frag_size,"&& $9<=",max_frag_size,") ||", "($9<=-",min_frag_size,"&& $9>=",max_frag_size,")' | \ samtools view -b >",paste0(ULPwgs::get_sample_name(file),"_",min_frag_size,"-",max_frag_size,"_multiple-pos.bam")))
-
-    }
-    system(paste(bin_path,"view -h -L",bed,file," | \ awk 'substr($0,1,1)==\"@\""," || ($9>=",min_frag_size,"&& $9<=",max_frag_size,") ||", "($9<=-",min_frag_size,"&& $9>=",max_frag_size,")' | \ samtools view -b >",paste0(ULPwgs::get_sample_name(file),"_",min_frag_size,"-",max_frag_size,"_multiple-pos.bam")))
+    return("ERROR. Multiple input methods selected")
   }
+
 
 
 }
